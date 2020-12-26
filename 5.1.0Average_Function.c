c73803361ab1e6165b91d1844912ff12c9028b81
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2020-12-26 23:59:29 +0530

    Update 5.1.0Average_Function.c

--- 5.1.0Average_Function.c
+++ 5.1.0Average_Function.c
@@ -2,16 +2,14 @@
 float average(float a,float b,float c);
 int main()
 {
-    float a,b,c,avg;//  taking the float variables   
+    float a,b,c,avg;
     printf("Enter 3 Numbers\n");
-    scanf("%f",&a);//taking the input
-    scanf("%f",&b);
-    scanf("%f",&c);
-    avg=average(a,b,c);// finding the average
+    scanf("%f",&a,&b,&c);
+    avg=average(a,b,c);
     printf("The Average of the Numbers entered above is %f",avg);
-    return 0;//returning main
-}
-float average(float a,float b,float c)//function to find average
+    return 0;
+
+   float average(float a,float b,float c)//function to find average
 {
     float avg=(a+b+c)/3.00;
     return avg;
