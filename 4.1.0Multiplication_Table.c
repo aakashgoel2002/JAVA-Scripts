6271820b072da9ed5f9c67065aa441da0d5c3de5
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-17 23:54:42 +0530

    Update 4.1.0Multiplication_Table.c

--- 4.1.0Multiplication_Table.c
+++ 4.1.0Multiplication_Table.c
@@ -2,7 +2,7 @@
 int main(){
     int n,i;
     printf("Enter the Number whose table is to be printed: ");
-    scanf("%d",& n);
+    scanf("%d",&n);
     for(i=1;i<=10;i++)
     {
         printf("%dx%d=%d\n",n,i,n*i);
