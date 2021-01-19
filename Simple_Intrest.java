948b263b36145b0f91dac1eeb60d4065b4e62fe6
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:38:58 +0530

    Update Simple_Intrest.java

--- Simple_Intrest.java
+++ Simple_Intrest.java
@@ -8,7 +8,7 @@ public class SimpleIntrest
         System.out.println("Enter Principal");
         principal=inp.nextInt();
         System.out.println("Enter Rate");
-        r=inp.nextInt();
+        rate=inp.nextInt();
         System.out.println("Enter Time");
         t=inp.nextInt();
         System.out.println("SI is"+(principal*r*t)/100);
