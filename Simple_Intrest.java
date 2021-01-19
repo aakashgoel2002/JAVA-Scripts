f72dccaadb14e33a63885749fda1a206544505e5
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:49:15 +0530

    Update Simple_Intrest.java

--- Simple_Intrest.java
+++ Simple_Intrest.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 public class SimpleIntrest
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Scanner inp=new Scanner(System.in);
         int principal,rate,time;
-        System.out.println("Enter Principal: ₹");
+        System.out.println("Enter Principal: ");
         principal=inp.nextInt();
         System.out.println("Enter Rate: ");
         rate=inp.nextInt();
