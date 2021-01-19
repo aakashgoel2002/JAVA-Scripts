a62d95de93fa0af25b869d04f06b7cd1a6472dd8
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:44:21 +0530

    Update Simple_Intrest.java

--- Simple_Intrest.java
+++ Simple_Intrest.java
@@ -11,6 +11,6 @@ public class SimpleIntrest
         rate=inp.nextInt();
         System.out.println("Enter Time(in months): ");
         time=inp.nextInt();
-        System.out.println("Simple Intrest is: "+(principal*rate*time)/100);
+        System.out.println("Simple Intrest : "+(principal*rate*time)/100);
     }
 }
