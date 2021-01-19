2d6a7ac8e0d37366082f13375d2d6594bf054a76
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:34:48 +0530

    Create Simple_Intrest.java

---
+++ Simple_Intrest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner inp= new Scanner(System.in);
+        int p,r,t;
+        System.out.println("Enter Principal");
+        p=inp.nextInt();
+        System.out.println("Enter Rate");
+        r=inp.nextInt();
+        System.out.println("Enter Time");
+        t=inp.nextInt();
+        System.out.println("SI is"+(p*r*t)/100);
+    }
+}
