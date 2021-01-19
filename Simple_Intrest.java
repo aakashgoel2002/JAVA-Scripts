299b27e1b21447dafa14974a5448976635efc78b
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:49:26 +0530

    Update Simple_Intrest.java

--- Simple_Intrest.java
+++ Simple_Intrest.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-public class SimpleIntrest
-{
+public class SimpleIntrest{
     public static void main(String[] args){
         Scanner inp=new Scanner(System.in);
         int principal,rate,time;
