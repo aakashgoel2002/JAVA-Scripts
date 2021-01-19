553a81339c0693e74361dbbd9eea99aec494cdfa
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-19 23:39:59 +0530

    Update Simple_Intrest.java

--- Simple_Intrest.java
+++ Simple_Intrest.java
@@ -4,7 +4,7 @@ public class SimpleIntrest
     public static void main(String[] args)
     {
         Scanner inp= new Scanner(System.in);
-        int principal,rate,t;
+        int principal,rate,time;
         System.out.println("Enter Principal");
         principal=inp.nextInt();
         System.out.println("Enter Rate");
