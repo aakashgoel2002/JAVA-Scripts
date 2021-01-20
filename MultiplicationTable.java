374f5bf3debe732e95dcc6754a0f482706a2d5cb
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-20 23:54:40 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,7 +3,7 @@ public class MultiplicationTable{
     public static void main(String[] args){
         Scanner inp=new Scanner(System.in);
         int number,i;
-        System.out.println("Enter the Number of which the table is to be printed: ");
+        System.out.println("Enter the Number of which the table is to be printed: ")
         number=inp.nextInt();
         for(i=1;i<=10;i++){
          System.out.println(number+"*"+i+"="+(number*i));   
