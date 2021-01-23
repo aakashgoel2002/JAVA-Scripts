7ac82df71a8e11d14fb167022146644dce9d0a0d
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-23 23:43:02 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class MultiplicationTable{
     public static void main(String[] args){
-        Scanner inp=new Scanner(System.in);
+        Scanner inp=new Scanner(System.in)
         int number,i;
         System.out.println("Enter the Number of which the table is to be printed: ");
         number=inp.nextInt();
