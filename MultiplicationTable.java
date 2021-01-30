802efa80f407342aec2fcf0aa5603037a024fdc5
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-30 23:16:18 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -5,7 +5,7 @@ public class MultiplicationTable{
         int number,i;
         System.out.println("Enter the Number of which the table is to be printed: ");
         number=inp.nextInt();
-        for(i=1;i<=10;i++
+        for(i=1;i<=10;i++)
         System.out.println(number+" * "+i+" = "+(number*i));
     }
 }
